db1b51a1597f0d036c1729ffdea1a19b406d44fa
clau <claudmatt@gmail.com>
2023-12-26 12:18:21 +0100

    Fixed horizontal scrollbar appearing  on small desktop sizes

--- app/components/ui/H2.tsx
+++ app/components/ui/H2.tsx
@@ -4,7 +4,7 @@ const bungeeOutline = Bungee_Outline({ subsets: ['latin'], weight: '400' })
 
 function H2 ({ children }: { children: React.ReactNode }): React.JSX.Element {
   return (
-    <h2 className={`${bungeeOutline.className} text-7xl text-center my-16`}>{children}</h2>
+    <h2 className={`${bungeeOutline.className} text-5xl xl:text-7xl text-center my-16`}>{children}</h2>
   )
 }
 
