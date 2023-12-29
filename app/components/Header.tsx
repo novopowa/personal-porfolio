fe9f9016c2ea9b859ca2450b54f5712a099af628
clau <claudmatt@gmail.com>
2023-12-29 21:42:43 +0100

    Main photo style adjustments on md screen size

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -8,7 +8,7 @@ const major = Major_Mono_Display({ subsets: ['latin'], weight: '400' })
 function Header({ h1, links, selectedPage }: { h1: string; links: LINKS; selectedPage: string }): React.JSX.Element {
 	return (
 		<header
-			className={`flex flex-col mt-0 mx-auto w-screen sm:w-[36rem] lg:w-auto lg:flex-[0_0_36rem] max-h-screen lg:sticky top-0 ${major.className}`}>
+			className={`flex flex-col mt-0 mx-auto w-screen sm:w-[36rem] lg:w-auto lg:flex-[0_0_36rem] lg:max-h-screen lg:sticky top-0 ${major.className}`}>
 			<Intro selectedPage={selectedPage} links={links} />
 			<Menu selectedPage={selectedPage} h1={h1} links={links} />
 		</header>
