f2e763a61d9a7bc1e728c7be99131ec0b9f89b6f
clau <claudmatt@gmail.com>
2024-01-16 18:46:07 +0100

    Added metatags

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,22 @@ import './globals.css'
 import type { Metadata } from 'next'
 
 export const metadata: Metadata = {
-	title: 'Aleix Alsina',
-	description: 'The portfolio of Aleix Alsina, Frontend Developer.'
+	title: 'Porfolio de ALEIX ALSINA - Frontend Developer',
+	description:
+		'Soy un técnico especializado en desarrollo web con más de 6 años de experiencia. Aquí podrás encontrar los proyectos en los que he trabajado a lo largo de mi carrera.',
+	applicationName: 'Porfolio de Aleix Alsina',
+	referrer: 'origin-when-cross-origin',
+	metadataBase: new URL('https://aleixalsina.vercel.app'),
+	appleWebApp: true,
+	openGraph: {
+		title: 'Porfolio de ALEIX ALSINA - Frontend Developer',
+		description:
+			'Soy un técnico especializado en desarrollo web con más de 6 años de experiencia. Aquí podrás encontrar los proyectos en los que he trabajado a lo largo de mi carrera.',
+		type: 'website',
+		url: new URL('https://aleixalsina.vercel.app'),
+		siteName: 'Porfolio de Aleix Alsina',
+		locale: 'es_ES'
+	}
 }
 
 export default function RootLayout({ children }: { children: React.ReactNode }): React.JSX.Element {
